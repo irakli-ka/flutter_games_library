862630761d20ca14db599da8af0d139ef75b879b
irakli-ka <irakli.kasradze.1@btu.edu.ge>
2026-06-25 18:33:37 +0400

    add firebase to android side

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,12 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.5.0")
+    }
+}
 allprojects {
     repositories {
         google()
